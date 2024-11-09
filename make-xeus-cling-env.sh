f0494362e7b46317944becd7ecbe9e9cef9f5c95
Armin Sobhani <arminms@gmail.com>
2024-11-09 15:45:26 -0500

    Remove temporary comments in the script

--- make-xeus-cling-env.sh
+++ make-xeus-cling-env.sh
@@ -1,8 +1,8 @@
 
 #!/usr/bin/env bash
 #
-# bash script for building/installing xeus-cling 0.15.3 from source in a virtual
-# environment
+# bash script for building/installing xeus-cling 0.15.3 from source in a python
+# virtual environment
 #
 # Copyright (c) 2024 Armin Sobhani (arminms@gmail.com)
 #
@@ -17,7 +17,7 @@ fi
 
 mkdir -p build && cd build
 cp -r kernels patches build
-export BUILD_DIR=$PWD
+BUILD_DIR=$PWD
 
 # exit on error and verbose
 set -ex
@@ -36,7 +36,6 @@ if [ ! -d "llvm-project" ]; then
   git clone https://github.com/root-project/llvm-project.git
 fi
 cd llvm-project/
-# git checkout cling-latest
 git checkout cling-llvm13
 
 # cling v1.0~dev
@@ -45,7 +44,6 @@ if [ ! -d "cling" ]; then
   git clone https://github.com/root-project/cling.git
 fi
 cd cling/
-# git checkout v0.9
 git checkout acb2334131c19ef506767d6d9051b24755a8566b
 # patch to allow redefinition by default
 sed -i -e 's/AllowRedefinition(0)/AllowRedefinition(1)/g' include/cling/Interpreter/RuntimeOptions.h
@@ -65,18 +63,9 @@ cmake -DLLVM_EXTERNAL_PROJECTS=cling \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
       -DCMAKE_BUILD_TYPE=Release \
       $BUILD_DIR/llvm-project/llvm
-#      -DLLVM_TOOL_OPENMP_BUILD=ON
-#      -DCMAKE_SYSTEM_PREFIX_PATH=$EBROOTGENTOO \
-#      -DLLVM_REQUIRES_EH=ON \
-#      -DLLVM_ENABLE_LIBCXX=ON \
 cmake --build . --target cling llvm-config -j 4
-make -j install-clang-headers install-clang-resource-headers install-llvm-headers # install-libcling install-cling-cmake-exports install-clang install-libclang install-libclang-headers install-clang-cpp install-llvm-config
-
+make -j install-clang-headers install-clang-resource-headers install-llvm-headers
 #export PATH=$INSTALL_DIR/bin:$PATH
-
-# copying necessary files
-# cp -r $BUILD_DIR/llvm-project/clang/include/clang $INSTALL_DIR/include
-# cp -r tools/clang/include/clang $INSTALL_DIR/include
 cp bin/* $INSTALL_DIR/bin
 cp lib/*.a $INSTALL_DIR/lib
 cp -r lib/cmake $INSTALL_DIR/lib
@@ -154,13 +143,9 @@ git clone https://github.com/jupyter-xeus/xeus-cling.git
 cd xeus-cling/
 git checkout 0.15.3
 
-# fix compilation errors with llvm16 (https://github.com/jupyter-xeus/xeus-cling/pull/524/files)
-# sed -i -e 's/"--src-root"//g' CMakeLists.txt
-# sed -i -e 's/HeaderSearchOpts,/&CI->getVirtualFileSystem(), HeaderSearchOpts,/g' src/xmagics/executable.cpp
-# sed -i -e 's/*Context)/*m_interpreter.getLLVMContext())/g' src/xmagics/executable.cpp
+# fix compilation errors with llvm13
 sed -i -e 's/.getDataLayout();/.getDataLayoutString()/g' src/xmagics/executable.cpp
 sed -i -e 's/getDataLayoutString()/getDataLayoutString();/g' src/xmagics/executable.cpp
-# sed -i -e 's/{llvm::NoneType::None,/std::nullopt,/g' src/xmagics/executable.cpp
 sed -i -e 's/simplisticCastAs/castAs/g' src/xmagics/execution.cpp
 sed -i -e 's/code.str()/std::string(code.str())/g' src/xmime_internal.hpp
 
