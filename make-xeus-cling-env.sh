e279eba7f52ac3587a1dee0719003117e0c1fe70
Armin Sobhani <arminms@gmail.com>
2024-11-13 17:24:24 -0500

    Refactor installation script to support resume functionality and improve conditional checks for dependencies

--- make-xeus-cling-env.sh
+++ make-xeus-cling-env.sh
@@ -107,42 +107,44 @@ else
   export PATH=$INSTALL_DIR/bin:$PATH
 fi
 
-# llvm13
+# llvm13 and cling v1.0~dev
 #
-cd $BUILD_DIR
-if [ ! -d "llvm-project" ]; then
-  git clone https://github.com/root-project/llvm-project.git
+if [ -z "${RESUME}" ] || ([ $RESUME -eq 1 ] && [ ! -f $BUILD_DIR/cling-build/bin/cling ]); then
+  cd $BUILD_DIR
+  if [ ! -d "llvm-project" ]; then
+    git clone https://github.com/root-project/llvm-project.git
+  fi
+  cd llvm-project/
+  git checkout cling-llvm13
+  cd $BUILD_DIR
+  if [ ! -d "cling" ]; then
+    git clone https://github.com/root-project/cling.git
+  fi
+  cd cling/
+  git checkout acb2334131c19ef506767d6d9051b24755a8566b
+  # patch to allow redefinition by default
+  sed -i -e 's/AllowRedefinition(0)/AllowRedefinition(1)/g' include/cling/Interpreter/RuntimeOptions.h
+  mkdir -p $BUILD_DIR/cling-build && cd $BUILD_DIR/cling-build
+  cmake -DLLVM_EXTERNAL_PROJECTS=cling \
+        -DLLVM_BUILD_LLVM_DYLIB=OFF \
+        -DLLVM_ENABLE_RTTI=ON \
+        -DLLVM_ENABLE_EH=ON \
+        -DLLVM_BUILD_DOCS=OFF \
+        -DLLVM_ENABLE_SPHINX=OFF \
+        -DLLVM_ENABLE_DOXYGEN=OFF \
+        -DLLVM_ENABLE_WERROR=OFF \
+        -DLLVM_EXTERNAL_CLING_SOURCE_DIR=$BUILD_DIR/cling/ \
+        -DLLVM_ENABLE_PROJECTS="clang" \
+        -DLLVM_TARGETS_TO_BUILD="host;NVPTX" \
+        -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
+        -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -DCMAKE_BUILD_TYPE=Release \
+        $BUILD_DIR/llvm-project/llvm
+  cmake --build . --target cling llvm-config -j $N
+  make -j install-clang-headers install-clang-resource-headers install-llvm-headers
+else
+  cd $BUILD_DIR/cling-build
 fi
-cd llvm-project/
-git checkout cling-llvm13
-
-# cling v1.0~dev
-#
-cd $BUILD_DIR
-if [ ! -d "cling" ]; then
-  git clone https://github.com/root-project/cling.git
-fi
-cd cling/
-git checkout acb2334131c19ef506767d6d9051b24755a8566b
-# patch to allow redefinition by default
-sed -i -e 's/AllowRedefinition(0)/AllowRedefinition(1)/g' include/cling/Interpreter/RuntimeOptions.h
-mkdir -p $BUILD_DIR/cling-build && cd $BUILD_DIR/cling-build
-cmake -DLLVM_EXTERNAL_PROJECTS=cling \
-      -DLLVM_BUILD_LLVM_DYLIB=OFF \
-      -DLLVM_ENABLE_RTTI=ON \
-      -DLLVM_ENABLE_EH=ON \
-      -DLLVM_BUILD_DOCS=OFF \
-      -DLLVM_ENABLE_SPHINX=OFF \
-      -DLLVM_ENABLE_DOXYGEN=OFF \
-      -DLLVM_ENABLE_WERROR=OFF \
-      -DLLVM_EXTERNAL_CLING_SOURCE_DIR=$BUILD_DIR/cling/ \
-      -DLLVM_ENABLE_PROJECTS="clang" \
-      -DLLVM_TARGETS_TO_BUILD="host;NVPTX" \
-      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
-      -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-      -DCMAKE_BUILD_TYPE=Release \
-      $BUILD_DIR/llvm-project/llvm
-cmake --build . --target cling llvm-config -j $N
 make -j install-clang-headers install-clang-resource-headers install-llvm-headers
 cp bin/* $INSTALL_DIR/bin
 cp lib/*.a $INSTALL_DIR/lib
@@ -153,93 +155,131 @@ cp -r $CH/Interpreter $CH/MetaProcessor $CH/UserInterface $CH/Utils $INSTALL_DIR
 
 # nlohmann/json v3.6.1
 #
-cd $BUILD_DIR
-git clone https://github.com/nlohmann/json.git
-cd json/
-git checkout v3.6.1
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_BUILD_TYPE=Release -DJSON_BuildTests=OFF
-cmake --build build -j && cmake --install build
+if [ -z "${RESUME}" ] || ([ $RESUME -eq 1 ] && [ ! -f $INSTALL_DIR/include/nlohmann/json.hpp ]); then
+  cd $BUILD_DIR
+  if [ ! -d "json" ]; then
+    git clone https://github.com/nlohmann/json.git
+  fi
+  cd json/
+  git checkout v3.6.1
+  cmake -S . -B build -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_BUILD_TYPE=Release -DJSON_BuildTests=OFF
+  cmake --build build -j && cmake --install build
+fi
 
 # xtl 0.7.5
 #
-cd $BUILD_DIR
-git clone https://github.com/xtensor-stack/xtl.git
-cd xtl/
-git checkout 0.7.5
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
-cmake --build build -j && cmake --install build
+if [ -z "${RESUME}" ] || ([ $RESUME -eq 1 ] && [ !  -d $INSTALL_DIR/include/xtl ]); then
+  cd $BUILD_DIR
+  if [ ! -d "xtl" ]; then
+    git clone https://github.com/xtensor-stack/xtl.git
+  fi
+  cd xtl/
+  git checkout 0.7.5
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+  cmake --build build -j && cmake --install build
+fi
 
 # xeus 3.2.0
 #
-cd $BUILD_DIR
-git clone https://github.com/jupyter-xeus/xeus.git
-cd xeus/
-git checkout 3.2.0
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
-cmake --build build -j && cmake --install build
+if [ -z "${RESUME}" ] || ([ $RESUME -eq 1 ] && [ ! -f $INSTALL_DIR/include/xeus/xeus.hpp ]); then
+  cd $BUILD_DIR
+  if [ ! -d "xeus" ]; then
+    git clone https://github.com/jupyter-xeus/xeus.git
+  fi
+  cd xeus/
+  git checkout 3.2.0
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+  cmake --build build -j && cmake --install build
+fi
 
 # libzmq v4.3.4
 #
-cd $BUILD_DIR
-git clone https://github.com/zeromq/libzmq.git
-cd libzmq/
-git checkout v4.3.4
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DWITH_PERF_TOOL=OFF -DZMQ_BUILD_TESTS=OFF
-cmake --build build -j && cmake --install build
+if [ -z "${RESUME}" ] || ([ $RESUME -eq 1 ] && [ ! -f $INSTALL_DIR/lib/libzmq.so ]); then
+  cd $BUILD_DIR
+  if [ ! -d "libzmq" ]; then
+    git clone https://github.com/zeromq/libzmq.git
+  fi
+  cd libzmq/
+  git checkout v4.3.4
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DWITH_PERF_TOOL=OFF -DZMQ_BUILD_TESTS=OFF
+  cmake --build build -j && cmake --install build
+fi
 
 # cppzmq v4.8.1
 #
-cd $BUILD_DIR
-git clone https://github.com/zeromq/cppzmq.git
-cd cppzmq/
-git checkout v4.8.1
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCPPZMQ_BUILD_TESTS=NO
-cmake --build build -j && cmake --install build
+if [ -z "${RESUME}" ] || ([ $RESUME -eq 1 ] && [ ! -f $INSTALL_DIR/share/cmake/cppzmq/cppzmqConfig.cmake ]); then
+  cd $BUILD_DIR
+  if [ ! -d "cppzmq" ]; then
+    git clone https://github.com/zeromq/cppzmq.git
+  fi
+  cd cppzmq/
+  git checkout v4.8.1
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCPPZMQ_BUILD_TESTS=NO
+  cmake --build build -j && cmake --install build
+fi
 
 # xeus-zmq 1.3.0
 #
-cd $BUILD_DIR
-git clone https://github.com/jupyter-xeus/xeus-zmq.git
-cd xeus-zmq/
-git checkout 1.3.0
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
-cmake --build build -j && cmake --install build
+if [ -z "${RESUME}" ] || ([ $RESUME -eq 1 ] && [ ! -f $INSTALL_DIR/lib/libxeus-zmq.so ]); then
+  cd $BUILD_DIR
+  if [ ! -d "xeus-zmq" ]; then
+    git clone https://github.com/jupyter-xeus/xeus-zmq.git
+  fi
+  cd xeus-zmq/
+  git checkout 1.3.0
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+  cmake --build build -j && cmake --install build
+fi
 
 # pugixml v1.8.1
 #
-cd $BUILD_DIR
-git clone https://github.com/zeux/pugixml.git
-cd pugixml/
-git checkout v1.8.1
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-cmake --build build -j && cmake --install build
+if [ -z "${RESUME}" ] || ([ $RESUME -eq 1 ] && [ ! -f $INSTALL_DIR/lib/libpugixml.a ]); then
+  cd $BUILD_DIR
+  if [ ! -d "pugixml" ]; then
+    git clone https://github.com/zeux/pugixml.git
+  fi
+  cd pugixml/
+  git checkout v1.8.1
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+  cmake --build build -j && cmake --install build
+fi
 
 # argparse v2.9
 #
-cd $BUILD_DIR
-git clone https://github.com/p-ranav/argparse.git
-cd argparse/
-git checkout v2.9
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
-cmake --build build -j && cmake --install build
+if [ -z "${RESUME}" ] || ([ $RESUME -eq 1 ] && [ ! -f $INSTALL_DIR/include/argparse/argparse.hpp ]); then
+  cd $BUILD_DIR
+  if [ ! -d "argparse" ]; then
+    git clone https://github.com/p-ranav/argparse.git
+  fi
+  cd argparse/
+  git checkout v2.9
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+  cmake --build build -j && cmake --install build
+fi
 
 # xeus-cling 0.15.3
 #
-cd $BUILD_DIR
-git clone https://github.com/jupyter-xeus/xeus-cling.git
-cd xeus-cling/
-git checkout 0.15.3
-# fix compilation errors with llvm13
-sed -i -e 's/.getDataLayout();/.getDataLayoutString()/g' src/xmagics/executable.cpp
-sed -i -e 's/getDataLayoutString()/getDataLayoutString();/g' src/xmagics/executable.cpp
-sed -i -e 's/simplisticCastAs/castAs/g' src/xmagics/execution.cpp
-sed -i -e 's/code.str()/std::string(code.str())/g' src/xmime_internal.hpp
-# patch to install new kernel files
-rm -rf share/jupyter/kernels/*
-cp -r $BUILD_DIR/kernels/* share/jupyter/kernels/
-patch -u CMakeLists.txt $BUILD_DIR/patches/kernels.diff
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCling_DIR=$INSTALL_DIR/tools/cling/lib/cmake/cling
-cmake --build build -j && cmake --install build
+if [ -z "${RESUME}" ] || ([ $RESUME -eq 1 ] && [ ! -f $INSTALL_DIR/bin/xcpp ]); then
+  cd $BUILD_DIR
+  if [ ! -d "xeus-cling" ]; then
+    git clone https://github.com/jupyter-xeus/xeus-cling.git
+    cd xeus-cling/
+    git checkout 0.15.3
+    # fix compilation errors with llvm13
+    sed -i -e 's/.getDataLayout();/.getDataLayoutString()/g' src/xmagics/executable.cpp
+    sed -i -e 's/getDataLayoutString()/getDataLayoutString();/g' src/xmagics/executable.cpp
+    sed -i -e 's/simplisticCastAs/castAs/g' src/xmagics/execution.cpp
+    sed -i -e 's/code.str()/std::string(code.str())/g' src/xmime_internal.hpp
+    # patch to install new kernel files
+    rm -rf share/jupyter/kernels/*
+    cp -r $BUILD_DIR/kernels/* share/jupyter/kernels/
+    patch -u CMakeLists.txt $BUILD_DIR/patches/kernels.diff
+  else
+    cd xeus-cling/
+  fi
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCling_DIR=$INSTALL_DIR/tools/cling/lib/cmake/cling
+  cmake --build build -j && cmake --install build
+fi
 
 # deactivate the virtual environment if needed
 #
